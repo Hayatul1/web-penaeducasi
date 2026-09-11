0424029b72537449fec50640cac19642ee64bf6c
Hayatul1 <makky300687@gmail.com>
2026-09-12 06:57:57 +0700

    fix: terapkan desain Pena SEO ke sitemap utama

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,18 +1,25 @@
 import { NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 export async function GET() {
   const baseUrl = 'https://web.penaeducasi.com';
+  const now = new Date().toISOString(); 
   
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap-style.xsl"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
     <loc>${baseUrl}/posts-sitemap.xml</loc>
+    <lastmod>${now}</lastmod>
   </sitemap>
   <sitemap>
     <loc>${baseUrl}/pages-sitemap.xml</loc>
+    <lastmod>${now}</lastmod>
   </sitemap>
   <sitemap>
     <loc>${baseUrl}/categories-sitemap.xml</loc>
+    <lastmod>${now}</lastmod>
   </sitemap>
 </sitemapindex>`;
 
